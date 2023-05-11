af6edcdeee90c1af1f95de6f8bfd2dc888dfac13
carlos3534 <carlosjosehenao@hotmail.com>
2023-05-11 09:48:24 -0500

    Carrusel (Ya se ve pero no se mueve(
    Hay que buscar como hacer que se muevan las imagenes automaticamente, arreglar header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import Banner from './Components/Banner.js';
 import Busqueda from './Components/Busqueda.js';
+import Carrusel from './Components/Carrusel';
 import Footer from './Components/Footer.js';
-
+import Carousel from './Components/Carousel';
 
 
 function App() {
 	return (
 		<div>
 			<Banner/>
+			<Carousel/>
 			<Footer/>
 		</div>
 	);
--- src/Components/Carrusel.js
+++ src/Components/Carrusel.js
@@ -1,5 +1,6 @@
 import React from "react";
-import Avatar from './img/Avatar.jpg';
+
+<script src="https://cdn.tailwindcss.com"></script>
 
 function Carrusel(){
     return (
@@ -9,23 +10,23 @@ function Carrusel(){
                 <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
 
                     <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                        <img src="C:\Users\LABORATORIO\Desktop\Carlos Henao\React\IAMGENES\Avatar.jpg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                        <img src={require(`../img/End Game.jpg`)} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
                     </div>
 
                     <div className="hidden duration-700 ease-in-out" data-carousel-item="active">
-                        <img src="/docs/images/carousel/carousel-2.svg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                        <img src={require(`../img/SAw.jpg`)} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
                     </div>
 
                     <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                        <img src="/docs/images/carousel/carousel-3.svg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                        <img src="/docs/images/carousel/carousel-3.svg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
                     </div>
  
                     <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                        <img src="/docs/images/carousel/carousel-4.svg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                        <img src="/docs/images/carousel/carousel-4.svg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
                     </div>
 
                     <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                        <img src="/docs/images/carousel/carousel-5.svg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                        <img src="/docs/images/carousel/carousel-5.svg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
                     </div>
                 </div>
 
@@ -42,7 +43,7 @@ function Carrusel(){
                     </span>
                 </button>
             </div>
-
+        
     );
 
 }
